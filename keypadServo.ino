1edbb99d9ac88dab6da30c8e9bf11c63e77b1a51
Francisco Cunha <francisccdn@gmail.com>
2019-08-05 10:58:53 -0300

    Corrigido erro de nomeclatura

--- keypadServo.ino
+++ keypadServo.ino
@@ -1,10 +1,10 @@
 #include <Keypad.h>
 #include <Servo.h>
 
-#define AZI_DOOR 11
+#define ALT_DOOR 11
 
-const byte ROWS = 4; 
-const byte COLS = 4; 
+const byte ROWS = 4;
+const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
@@ -13,42 +13,43 @@ char hexaKeys[ROWS][COLS] = {
   {'*', '0', '#', 'D'}
 };
 
-byte rowPins[ROWS] = {6, 7, 8, 9}; 
-byte colPins[COLS] = {5, 4, 3, 2}; 
+byte rowPins[COLS] = {6, 7, 8, 9};
+byte colPins[ROWS] = {2, 3, 4, 5};
 
 char inKey[3];
 byte inKeyIndex = 0;
 
 byte numKey;
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-Servo azimuth;
+Servo altitude;
 
 void setup(){
   Serial.begin(9600);
-  azimuth.attach(AZI_DOOR);
+  altitude.attach(ALT_DOOR);
 }
-  
+
 void loop(){
   char readKey = customKeypad.getKey();
-  
+
   if (readKey){
-    inKey[inKeyIndex] = readKey; 
-    inKeyIndex++; 
+    inKey[inKeyIndex] = readKey;
+    inKeyIndex++;
   }
 
   if (inKeyIndex == 3){
     numKey = atol(inKey);
     Serial.print(numKey);
-    azimuth.write(numKey);
+    Serial.print("\n");
+    altitude.write(numKey);
     ClearData();
   }
 }
 
 void ClearData(){
   while(inKeyIndex != 0){
-    inKey[inKeyIndex--] = 0; 
+    inKey[inKeyIndex--] = 0;
   }
   return;
 }

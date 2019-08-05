7f624d9778df81a9178a86b5c3a800b0f72ee98c
Francisco Cunha <francisccdn@gmail.com>
2019-08-05 10:30:27 -0300

    Codigo mias legivel

--- motor_de_passo.ino
+++ motor_de_passo.ino
@@ -1,4 +1,5 @@
 #include <Stepper.h>
+
 #define E1 8
 #define E2 9 
 #define E3 10
@@ -6,14 +7,14 @@
 
 const int passosPorGiro = 32;
 
-Stepper Mp(passosPorGiro,E1,E3,E2,E4);
+Stepper Mp(passosPorGiro, E1, E3, E2, E4);
 
 void setup() {
 
- }
+}
 
 void loop() {
-motor(650,1,5);
+motor(650, 1, 5);
 //motor(650,-1,1);
 //motor(800,1,1);
 //motor(650,1,3);
@@ -25,10 +26,10 @@ while(1){
 
 void motor(int vel, int sentido , float voltas){
     Mp.setSpeed(vel); // RPM
-   for(int i = 1;i<=64*voltas;i++){
     
-   Mp.step(passosPorGiro*sentido);
-  }
+    for(int i = 1; i<=64*voltas; i++){
+        Mp.step(passosPorGiro*sentido);
+    }
   
-  delay(5000);
+    delay(5000);
  }

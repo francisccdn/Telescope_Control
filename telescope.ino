f5749706e920b76310ac5ffcc06e87a05f4f765a
Francisco Cunha <francisccdn@gmail.com>
2019-08-10 09:52:48 -0300

    Motor de passos agora gira nas duas direções e melhorias gerais

--- telescope.ino
+++ telescope.ino
@@ -4,10 +4,10 @@
 #include <Servo.h>
 
 #define ALT_DOOR 5
-#define E1 8
-#define E2 9
-#define E3 10
-#define E4 11
+#define E1 47
+#define E2 49
+#define E3 51
+#define E4 53
 
 const byte ROWS = 4;
 const byte COLS = 4;
@@ -18,9 +18,10 @@ char hexaKeys[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-
-byte rowPins[COLS] = {6, 7, 8, 9};
-byte colPins[ROWS] = {2, 3, 4, 5};
+//                    r1  r2  r3  r4
+byte rowPins[COLS] = {32, 34, 36, 38};
+//                    c1  c2  c3  c4 
+byte colPins[ROWS] = {28, 26, 24, 22}; 
 
 char inKey[3];
 byte inKeyIndex = 0;
@@ -35,6 +36,8 @@ const int passosPorGiro = 32;
 
 byte currentAngle = 0; //Guarda o ultimo input de angulo para reajustar para o proximo de acordo
 
+float coefAngular;
+
 Stepper azimuth(passosPorGiro, E1, E3, E2, E4);
 
 Servo altitude;
@@ -44,7 +47,6 @@ void setup() {
     altitude.attach(ALT_DOOR);
     lcd.init();
     lcd.backlight();
-    lcd.cursor();
     LcdReset();
 }
 
@@ -57,24 +59,35 @@ void loop() {
     inKeyIndex++;
   }
 
+
 //Manda o input para o motor
 
   if (indicador == 0 && inKeyIndex == 3){
     numKey = atol(inKey);
+
+    coefAngular = (numKey - currentAngle)/360.0;
 
     lcd.print("º");
     lcd.setCursor(11, 1);
 
     Serial.print(numKey);
     Serial.print("º\n");
+    Serial.print(" DIGITADO \n");
+    Serial.print(coefAngular);
+    Serial.print(" PARAMETRO \n");
 
-    Motor(650, 1, (numKey - currentAngle)/360 );
-
+    if(coefAngular < 0){ //Permite que o motor gire nas duas direções
+        Motor(650, -1, coefAngular * -1);
+    }else{
+        Motor(650, 1, coefAngular);
+    }
+    
     currentAngle = numKey;
     ClearData();
 
     indicador = 1; //Indica que o motor recebeu o dado
   }
+
 
 //Manda o input para o servo
 
@@ -82,14 +95,17 @@ void loop() {
     numKey = atol(inKey);
 
     lcd.print("º");
+    lcd.noCursor();
+    
     Serial.print(numKey);
-    Serial.print("\n");
+    Serial.print(" SERVO \n");
 
     altitude.write(numKey);
     ClearData();
 
     indicador = 2; //Indica que o servo recebeu o dado
   }
+
 
 /*Reseta o LCD quando uma nova tecla é pressionada
 (sem isso, o LCD resetaria automaticamente)*/
@@ -113,6 +129,7 @@ void Motor(int vel, int sentido , float voltas){
 
 void LcdReset(){
   lcd.clear();
+  lcd.cursor();
   lcd.print("Azimuth  : ");
   lcd.setCursor(0, 1);
   lcd.print("Altitude : ");
